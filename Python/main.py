d1feb64d6fd08843fb966d5966bed0a874c31c96
pr249 <pr249@PEDAGUEZ.local>
2021-11-04 18:47:48 -0600

    Actualizar .gitignore, Python.iml y 11 más archivos...

--- Python/main.py
+++ Python/main.py
@@ -1,16 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route('/')
 def inicio():
-    return render_template('index.html')
-@app.route('/login')
-def login():
-    return render_template('login.html')
+    return render_template('Index.html')
 
-@app.route('/principal')
+@app.route('/Login')
+def login():
+    return render_template('Login.html')
+
+@app.route('/PaginaPrincipal')
 def menu():
-    return render_template('principal.html')
+    return render_template('PaginaPrincipal.html')
+
+@app.route('/VMV')
+def VMV():
+    return render_template('VMV.html')
+@app.route('/Contactos')
+def Cont():
+    return render_template('Contactos.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
