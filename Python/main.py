c4dedc48dec8b4c4c0a7d7f99ec591d9704458b1
pr249 <pr249@PEDAGUEZ.local>
2021-11-04 19:50:24 -0600

    Actualizar main.py, AVISOPTC.html y 4 más archivos...

--- Python/main.py
+++ Python/main.py
@@ -16,9 +16,19 @@ def menu():
 @app.route('/VMV')
 def VMV():
     return render_template('VMV.html')
+
+@app.route('/AVISOPTC')
+def aviso():
+    return render_template('AVISOPTC.html')
+
 @app.route('/Contactos')
 def Cont():
     return render_template('Contactos.html')
+
+@app.route('/RedesSociales')
+def Redes():
+    return render_template('RedesSociales.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
